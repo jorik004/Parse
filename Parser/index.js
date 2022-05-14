fff32e496b66ab9a0e79cfbcc9cd5fdac0fe08a7
WhiteLord <57297036+jorik004@users.noreply.github.com>
2022-05-14 17:24:26 +0600

    Update index.js

--- Parser/index.js
+++ Parser/index.js
@@ -1,12 +1,15 @@
-const cheerio = require('cheerio')
-const axios = require('axios')
-const fs = require('fs')
+const cheerio = require('cheerio');
+const axios = require('axios');
+const fs = require('fs');
 
-const parse = async () => {
-
-    const url = 'https://restoran.kz/restaurant'
-    const page = 3
-
+(async () => {
+    
+    //LINK website
+    const url = 'URL'
+    //Page count
+    const page = 5
+    
+    //Getting html tags into an object
     async function getHtml(url) {
         const { data } = await axios.get(url)
         return cheerio.load(data)
@@ -15,27 +18,21 @@ const parse = async () => {
     const $ = await getHtml(url)
 
     for(let i = 1; i <= page; i++){
-        const selector = await getHtml(`https://2gis.kz/almaty/search/%D0%90%D0%B2%D1%82%D0%BE%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81%D1%8B%20(%D0%A1%D0%A2%D0%9E)/rubricId/9041/page/${i}`)
+        const selector = await getHtml(`url/${i}`)
         let title
         let number
 
-        // selector('.place-list-card__body').each((index, elem) => {
-        //     title = selector(elem).find('.link-inherit-color').text()
-        //     number = `${selector(elem).find('.phone-prefix').text()} ${selector(elem).find('.phone-code').text()} ${selector(elem).find('.phone-number').text()}`
-        //     fs.appendFileSync('./db.txt', `${title}       ${number} \n`)
-        // })
-        // selector('.phone').each((index, elem) => {
-        //     number = `${selector(elem).find('.phone-prefix').text()} ${selector(elem).find('.phone-code').text()} ${selector(elem).find('.phone-number').text()}`
-        //     fs.appendFileSync('./db.txt', `    ${number} \n`)
-        // })
-
-        selector('._1h3cgic').each((index, elem) => {
-            title = selector(elem).find('._hc69qa').text()
-            console.log(title)
+        selector('.place-list-card__body').each((index, elem) => {
+            title = selector(elem).find('.link-inherit-color').text()
+            number = `${selector(elem).find('.phone-prefix').text()} ${selector(elem).find('.phone-code').text()} ${selector(elem).find('.phone-number').text()}`
+            fs.appendFileSync('./db.txt', `${title}       ${number} \n`)
+        })
+        selector('.phone').each((index, elem) => {
+            number = `${selector(elem).find('.phone-prefix').text()} ${selector(elem).find('.phone-code').text()} ${selector(elem).find('.phone-number').text()}`
+            fs.appendFileSync('./db.txt', `    ${number} \n`)
         })
     }
     
 
-}
+})()
 
-parse()
